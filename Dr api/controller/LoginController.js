6a797f2f72d4a229ee49fd5808ded37262e67770
kiranmeshiya <kiranmeshiya@gmail.com>
2023-05-22 11:59:18 +0530

    full stack website of hospital with backend

---
+++ Dr api/controller/LoginController.js
@@ -0,0 +1,52 @@
+
+const loginmodel = require('../model/LoginModel');
+
+
+const Register = async (req,res) => {
+    try {
+        
+        var data = await loginmodel.create(req.body);
+        
+        res.status(200).json({
+            status:"Success",
+            data
+        })
+
+    } catch (error) {
+        
+        res.status(200).json({
+            status:"Error",
+            error
+        })
+    }
+}
+
+const Login = async (req,res) => {
+    
+    try {
+        
+        // var data = await loginmodel.find({email:req.body.email});
+        var data = await loginmodel.find({name:req.body.name});
+
+        if(data[0].password == req.body.password)
+        {
+            res.status(200).json({
+                status:"Login Success",
+                data
+            })
+        }
+
+    } catch (error) {
+        
+        res.status(200).json({
+            status:"Error",
+            error
+        })
+    }
+
+}
+
+module.exports = {
+    Register,
+    Login
+}
